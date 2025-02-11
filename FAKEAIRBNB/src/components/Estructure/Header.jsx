444df5a1fb3eca554cf043f0e0c7d69249164e30
Nicolas Tobon <nicolastobon242>
2025-02-10 23:42:21 -0500

    integracion de ruta pincipal en el logo del header

--- FAKEAIRBNB/src/components/Estructure/Header.jsx
+++ FAKEAIRBNB/src/components/Estructure/Header.jsx
@@ -1,18 +1,16 @@
+import { Link } from "react-router-dom";
 import "./header.css";
 import Login from "../../pages/RegistroInicio/Login";
 
-
 const Header = () => {
+  return (
+    <header className="header">
+      <Link to="/" className="logo">FakeAirbnb</Link>
+      <input type="text" placeholder="¿A dónde vas?" className="search-bar" />
+      <button className="prop-option">Pon tu espacio en Airbnb</button>
+      <Login />
+    </header>
+  );
+};
 
-    return (
-      <header className="header">
-        <div className="logo"> FakeAirbnb </div>
-        <input type="text" placeholder="¿A dónde vas?" className="search-bar" />
-        <button  className="prop-option">Pon tu espacio en Airbnb </button>
-        <Login />
-      </header>
-    );
-  };
-  
 export default Header;
-  
