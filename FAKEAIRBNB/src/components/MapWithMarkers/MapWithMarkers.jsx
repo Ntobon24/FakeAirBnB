50e6879d598f4276089a9a9564a0c03ebcdd992d
Nicolas Tobon <nicolastobon242>
2025-02-10 23:49:31 -0500

    correccion de error en mapa y funcion de dar click en marcadores añadida

--- FAKEAIRBNB/src/components/MapWithMarkers/MapWithMarkers.jsx
+++ FAKEAIRBNB/src/components/MapWithMarkers/MapWithMarkers.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import { getDocs, collection } from 'firebase/firestore';
-import { db } from '../../firebase/firebaseConfig'; // Asegúrate de importar tu configuración de Firebase
+import { db } from '../../firebase/firebaseConfig';
+import { useNavigate } from 'react-router-dom';
 
 const containerStyle = {
   width: '100%',
@@ -16,11 +17,15 @@ const center = {
 const MapWithMarkers = () => {
   const [map, setMap] = useState(null);
   const [propiedades, setPropiedades] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       const querySnapshot = await getDocs(collection(db, "propiedades"));
-      const propiedadesArray = querySnapshot.docs.map(doc => doc.data());
+      const propiedadesArray = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
       setPropiedades(propiedadesArray);
     };
     
@@ -32,22 +37,23 @@ const MapWithMarkers = () => {
   };
 
   return (
-    <LoadScript googleMapsApiKey={import.meta.env.VITE_FIREBASE_API_KEY}>
+    <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
         zoom={12}
         onLoad={onLoad}
       >
-        {propiedades.map((propiedad, index) => (
+        {propiedades.map((propiedad) => (
           <Marker
-            key={index}
+            key={propiedad.id}
             position={{
               lat: parseFloat(propiedad.lat),
               lng: parseFloat(propiedad.lng)
             }}
             title={propiedad.titulo}
-          />
+            onClick={() => navigate(`/reserva/${propiedad.id}`)} 
+            />
         ))}
       </GoogleMap>
     </LoadScript>
